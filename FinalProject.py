9d65df7b1d98439ce709e4f09d12b01b97cee2b5
Annie Slabotsky <aslabot@umich.edu>
2017-11-28 21:45:56 -0500

    struggling

--- FinalProject.py
+++ FinalProject.py
@@ -6,10 +6,6 @@ import json
 import sqlite3
 import requests
 
-changetoken = "EAACEdEose0cBAEXs3CrjUHMgmZC6eIYG38OEBmrh7TtuWPNmBEBCqvPVp1qpgqtOdUUWMiTarl4gsqnXRBveFAf3mv4MUXIfH2sCzypahunAFlSUOHacP7ISKnJg3SREEaILZB4ndsasZB9yR0qdXVIgaBRwWvzFYr6osk1GzuFW06AlvJg9UFXGvZBSyZB8njTZByeRIMSAZDZD"
-access_token = changetoken
-if access_token == None:
-    access_token = input ("generate token from https://developers.facebook.com/tools/explorer")
 
 CACHE_FNAME = "facebook_cache.json"
 try:
@@ -26,24 +22,57 @@ except:
 
 
 def get_FB_data():
-    if "me" in CACHE_DICTION:
+
+    if "200" in CACHE_DICTION:
         print('using cached data')
-        FB_results = CACHE_DICTION['me']
+        FB_results = CACHE_DICTION["200"]
+
     else:
         print('getting data from internet')
-        url = "https://graph.facebook.com/v2.11/me/feed"
+
+        access_token = None
+        if access_token == None:
+            access_token = input ("generate token from https://developers.facebook.com/tools/explorer\n")
+
+        url = "https://graph.facebook.com/v2.11/me/posts"
         urlparams = {}
         urlparams["access_token"] = access_token
-        urlparams["fields"] = "likes"
+        #urlparams["fields"] = "created_time,message,likes,comments,reactions,type"
+        urlparams["fields"] = "created_time,comments,type"
         urlparams["limit"] = 100
+
         request = requests.get(url, urlparams)
-        print (request.url)
+# you need to generate a new access token frequently because that is how facebook is set up
+        while request.status_code != 200:
+            access_token = input ("generate new access token from https://developers.facebook.com/tools/explorer\n")
+
+            urlparams["access_token"] = access_token
+            request = requests.get(url, urlparams)
+
+        code = str(request.status_code)
         FB_results = json.loads(request.text)
-        CACHE_DICTION['me'] = FB_results
+        #create cache file with FB stuff
+        CACHE_DICTION[code] = FB_results
         f = open(CACHE_FNAME, 'w')
         f.write(json.dumps(CACHE_DICTION))
         f.close()
+
+    # print (FB_results)
     return FB_results
+
+def write_to_DB(FB_results):
+    #print("abc")
+    for item in FB_results:
+        #print("==============================\n")
+        tup = item["id"], item["name"], item["type"], item["created_time"]
+        #print(tw)
+        tw_id =  tw["id"]
+        #print(type(tw_id))
+    cur.execute('INSERT INTO ITEMS (id, name, type, created_time) VALUES (?, ?, ?, ?)', tup)
+#  5- Use the database connection to commit the changes to the database
+
+    conn.commit()
+
 
 
 if __name__ == "__main__":
